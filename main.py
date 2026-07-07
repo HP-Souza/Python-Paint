eb54531f8ec65c100afab6ff774efd88dc140353
HP-Souza <hpsouza@proton.me>
2026-07-06 22:23:29 -0300

    feat: Adição de salvamento e carregamento de desenhos

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from Views.CanvasView import CanvasView
 from Models.Desenhos import Desenhos
 from Controllers.CanvasController import CanvasController
 
-
 janela = tk.Tk()
 model = Desenhos()
+model.carregar()
+
 view = CanvasView(janela, model)
 controller = CanvasController(model, view)
 view.set_controller(controller)
+
+def on_close():
+    model.salvar()
+    janela.destroy()
+
+janela.protocol("WM_DELETE_WINDOW", on_close)
 janela.mainloop()
